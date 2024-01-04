988262a0b2390e0d58288665f56ffdc4ce17f880
jpsouza-rod <jpsouza.rod@gmail.com>
2024-01-04 11:50:13 -0300

    primeiro commit

---
+++ 1 - Hello World/Receive/Receive/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "amq.gen-JpwzeGAjzz1Gc-lAf9Ufpw");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+channel.BasicConsume(queue: "amq.gen-JpwzeGAjzz1Gc-lAf9Ufpw",
+                     autoAck: true,
+                     consumer: consumer);
+
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
